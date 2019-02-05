a5183b21d801036a6cfcf299d605e04de1ee2842
Daniel Tschan <tschan@puzzle.ch>
2019-02-05 15:48:32 +0100

    Allow to configure target user.

--- src/libmapuid.c
+++ src/libmapuid.c
@@ -21,20 +21,29 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <dlfcn.h>
+#include <stdlib.h>
 
 static struct passwd* (*next_getpwuid)(uid_t uid);
-static int (*next_getpwuid_r)(uid_t uid, struct passwd *pwd, char *buf, size_t buflen, struct passwd **result);
+static uid_t (*next_getpwuid_r)(uid_t uid, struct passwd *pwd, char *buf, size_t buflen, struct passwd **result);
+
+static int target_user = 1001;
 
 void libmapuid_init (void) __attribute__((constructor));
 void libmapuid_init (void)
 {
+  char* mapuid_target_user;
+
 	next_getpwuid = dlsym(RTLD_NEXT, "getpwuid");
 	next_getpwuid_r = dlsym(RTLD_NEXT, "getpwuid_r");
+  mapuid_target_user = getenv("MAPUID_TARGET_USER");
+  if (mapuid_target_user) {
+    target_user = atoi(mapuid_target_user);
+  }
 }
 
 struct passwd *getpwuid(uid_t uid) {
   if (uid >= 1000000000) {
-    uid = 1001;
+    uid = target_user;
   }
 
   return next_getpwuid(uid);
@@ -42,7 +51,7 @@ struct passwd *getpwuid(uid_t uid) {
 
 int getpwuid_r(uid_t uid, struct passwd *pwd, char *buf, size_t buflen, struct passwd **result) {
   if (uid >= 1000000000) {
-    uid = 1001;
+    uid = target_user;
   }
 
   return next_getpwuid_r(uid, pwd, buf, buflen, result);
